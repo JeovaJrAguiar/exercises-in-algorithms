448c498a37241bebaebbd9c4f605512eecb04eb1
JeovaJrAguiar <jeova.junior897@gmail.com>
2022-11-18 14:36:21 -0300

    .

---
+++ algorithm-04.c
@@ -0,0 +1,18 @@
+// the weekend
+#include <stdio.h>
+
+int main(){
+		printf("\n===== The Weekend- C =====");
+		printf("\n See prevision time.");
+		
+		printf("\n If(to be sun)");
+		printf("\n 	to go beach");
+		printf("\n Else");
+		printf("\n 	to go study");
+		
+		printf("\n To lunch");
+		printf("\n See TV");
+		printf("\n To sleep");
+		
+		return 0;
+}
---
+++ algorithm-04.py
@@ -0,0 +1,14 @@
+# the weekend
+
+print("===== The Weekend- C =====");
+print("See prevision time.");
+		
+print("If(to be sun)");
+print(" to go beach.");
+print("Else");
+print(" to go study.");
+		
+print("To lunch.");
+print("See TV.");
+print("To sleep.");
+
