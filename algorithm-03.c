354aabe3f67d059d6921d881010444bc34004bdc
JeovaJrAguiar <jeova.junior897@gmail.com>
2022-11-18 12:58:46 -0300

    .

---
+++ algorithm-03.c
@@ -0,0 +1,23 @@
+// Chenge the lamp
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	printf("\n===== Change the lamp - C =====");
+	
+	printf("\n If(lamp is really distant)");
+	printf("\n    get a stairs");
+	
+	printf("\n Get lamp");
+	
+	printf("\n If(lamp is warm)");
+	printf("\n    get a cloth");
+	
+	printf("\n If(lamp is berned)");
+	printf("\n    change the lamp\n");
+	
+	return 0;
+}
+
---
+++ algorithm-03.py
@@ -0,0 +1,14 @@
+# change the lamp
+
+print("===== Change the lamp - Python =====");
+	
+print(" If(lamp is really distant)");
+print("    get a stairs");
+	
+print(" Get lamp");
+	
+print(" If(lamp is warm)");
+print("    get a cloth");
+	
+print(" If(lamp is berned)");
+print("    change the lamp\n");
