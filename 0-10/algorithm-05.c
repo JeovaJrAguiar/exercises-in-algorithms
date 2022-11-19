885f0ce9f4aec3a71c8f0cb71f14ffa448400a41
JeovaJrAguiar <jeova.junior897@gmail.com>
2022-11-19 19:54:45 -0300

    To make a cake - algorithms

---
+++ 0-10/algorithm-05.c
@@ -0,0 +1,25 @@
+// To make a cake
+// Aguiar - computer science
+// University Federal of Ceará - campus Crateus
+
+#include <stdio.h>
+
+int main(){
+	printf("\n===== Make a cake - C =====");
+	printf("\nGet the ingredient.");
+	
+	printf("\nIf(clothes)");
+	printf("\n	put on apron");
+	
+	printf("\nIf(have a mixer)");
+	printf("\n	teat the ingredients on the mixer");
+	printf("\nElse");
+	printf("\n	teat the ingredient with hand");
+	
+	printf("\nPut on massa on the form");
+	printf("\nPut on form in the forn");
+	printf("\nWait required time");
+	printf("\nGet a cake\n");
+
+	return 0;
+}
---
+++ 0-10/algorithm-05.py
@@ -0,0 +1,20 @@
+# To make a cake
+# Aguiar - computer science
+# University Federal of Ceara - campus Crateus
+
+print("===== Make a cake - C =====");
+print("Get the ingredient.");
+
+print("If(clothes)");
+print("	put on apron");
+
+print("If(have a mixer)");
+print("	teat the ingredients on the mixer");
+print("Else");
+print("	teat the ingredient with hand");
+	
+print("Put on massa on the form");
+print("Put on form in the forn");
+print("Wait required time");
+print("Get a cake");
+
