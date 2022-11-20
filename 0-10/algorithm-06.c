a1d41aec1defa4960f07ad8caa0542831e9d3ff1
JeovaJrAguiar <jeova.junior897@gmail.com>
2022-11-20 13:15:18 -0300

    peel-potato

---
+++ 0-10/algorithm-06.c
@@ -0,0 +1,15 @@
+// Teel potato
+// Aguiar - computer science
+// University Federal of Ceará - campus Crateus
+
+#include <stdio.h>
+
+int main(){
+	printf("\n===== Peel potato - C =====");
+	printf("\nGet a knife, potato and basin.");
+	printf("\nPut on water in the basin");
+	printf("\nWhile(exist Potato)");
+	printf("\n	peel potato\n");
+	
+	return 0;
+}
---
+++ 0-10/algorithm-06.py
@@ -0,0 +1,9 @@
+# Peel potato
+# Aguiar - computer science
+# University Federal of Ceara - campus Crateus
+
+print("===== To peel potato - Puthon =====");
+print("Get a knife, potato and basin.");
+print("Put on water in the basi");
+print("While(exist Potato)");
+print("	peel potato.");
