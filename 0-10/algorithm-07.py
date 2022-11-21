7d9ce079ab7dbc126cb5076dfa1f70c72005fda6
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-11-21 08:51:53 -0300

    taking a test - Python

---
+++ 0-10/algorithm-07.py
@@ -0,0 +1,14 @@
+# Taking a test
+# Aguiar - computer science
+# University Federal of Ceará - campus Crateus
+
+print("===== Taking a test - Python =====");
+print("Read test.");
+print("Get pen.");
+print("While((there is white question) and (time is not over))");
+
+print("--If(know the question)");
+print("----to resolve.");
+
+print("--Else");
+print("----jump to other.");
