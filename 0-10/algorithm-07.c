6801f0b5153d0dc1085c62deeb423b18822472af
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-11-21 08:51:03 -0300

    taking a test - C

---
+++ 0-10/algorithm-07.c
@@ -0,0 +1,20 @@
+// Taking a test
+// Aguiar - computer science
+// University Federal of Ceará - campus Crateus
+
+#include <stdio.h>
+
+int main() {
+    printf("\n===== Taking a test - C =====");
+    printf("\nRead test.");
+    printf("\nGet pen.");
+    printf("\nWhile((there is white question) and (time is not over))");
+    
+    printf("\n  If(know the question)");
+    printf("\n      to resolve.");
+    
+    printf("\n  Else");
+    printf("\n      jump to other.\n");
+
+    return 0;
+}
