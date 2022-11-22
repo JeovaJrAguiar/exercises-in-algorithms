1b064ca4eab0883bf5df76756d12af5bf78dd504
JeovaJrAguiar <jeova.junior897@gmail.com>
2022-11-22 11:45:54 -0300

    .

---
+++ 0-10/algorithm-08.c
@@ -0,0 +1,18 @@
+// Gallows Game
+// Aguiar - computer science
+// University Federal of Ceará - campus Crateus
+
+#include <stdio.h>
+
+int main(){
+	printf("\n===== Gallows Game - C =====");
+	printf("\nChoice a word.m");
+	printf("\nsetup game diagram.");
+	printf("\nWhile((exist column empty) and (incomplete body))");
+	printf("\n	If(hit a letter)");
+	printf("\n		write in the gap");
+	printf("\n	Else");
+	printf("\n		to draw a body part in gallows\n");
+	
+	return 0;
+}
---
+++ 0-10/algorithm-08.py
@@ -0,0 +1,12 @@
+# Gallows Game
+# Aguiar - computer science
+# University Federal of Ceara - campus Crateus
+
+print("===== Gallows Game - Python =====");
+print("Choice a word.m");
+print("setup game diagram.");
+print("While((exist column empty) and (incomplete body))");
+print("	If(hit a letter)");
+print("		write in the gap");
+print("	Else");	
+print("		to draw a body part in gallows");
