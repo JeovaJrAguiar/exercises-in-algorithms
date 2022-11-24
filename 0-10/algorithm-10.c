cf6032b6a2240997e66dd64f64954d1fa57e363a
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-11-24 10:32:12 -0300

    Goat-lion-and-grass

---
+++ 0-10/algorithm-10.c
@@ -0,0 +1,19 @@
+// Goat, lion and grass
+// Aguiar - computer science
+// University Federal of Ceará - campus Crateus
+
+#include <stdio.h>
+
+int main(){
+    printf("\n===== Goat, lion and grass - C =====");
+    printf("\nTo take a grass and lion");
+    printf("\nCome back with lion");
+    printf("\nLeave lion");
+    printf("\nTo take goat");
+    printf("\nLeave goat");
+    printf("\nCome back with grass");
+    printf("\nTo take the lion and a grass");
+    printf("\n");
+    
+    return 0;
+}
---
+++ 0-10/algorithm-10.py
@@ -0,0 +1,12 @@
+# Goat, lion and grass
+# Aguiar - computer science
+# University Federal of Ceará - campus Crateus
+
+print("===== Goat, lion and grass - Python =====")
+print("To take a grass and lion")
+print("Come back with lion")
+print("Leave lion")
+print("To take goat")
+print("Leave goat")
+print("Come back with grass")
+print("To take the lion and a grass")
