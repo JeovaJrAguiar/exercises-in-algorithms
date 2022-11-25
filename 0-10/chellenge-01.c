cc47acf117275077431a309af1956d0f4f8259b6
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-11-25 10:19:30 -0300

    3-missionary-3-cannibals

---
+++ 0-10/chellenge-01.c
@@ -0,0 +1,15 @@
+// 3 missionary and 3 cannibals
+// Aguiar - computer science
+// University Federal of Ceará - campus Crateus
+
+#include <stdio.h>
+
+int main(){
+    printf("\n===== Chellenge - 3 missionary and 3 cannibals - C =====");
+    printf("\nWhile(exist cannibal and missionary in left margin)");
+    printf("\n  to take a cannibal and a missionary to the right margin");
+    printf("\n  can back up to the left margin");
+    printf("\n");
+
+    return 0;
+}
---
+++ 0-10/chellenge-01.py
@@ -0,0 +1,8 @@
+# 3 missionary and 3 cannibals
+# Aguiar - computer science
+# University Federal of Ceará - campus Crateus    
+
+print("===== Chellenge - 3 missionary and 3 cannibals - C =====")
+print("While(exist cannibal and missionary in left margin)")
+print("  to take a cannibal and a missionary to the right margin")
+print("  can back up to the left margin")
