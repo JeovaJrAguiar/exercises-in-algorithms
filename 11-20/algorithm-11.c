faa5b2d64b7cf7964a59ec2988670d4750d59026
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-12-21 14:28:53 -0300

    .

---
+++ 11-20/algorithm-11.c
@@ -0,0 +1,13 @@
+// Learning Algorithms
+// Aguiar - computer science
+// University Federal of Ceará - campus Crateus
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("\n===== Learning Algorithms - C =====");
+    printf("\nLearning algorithm.")
+
+    return 0;
+}
---
+++ 11-20/algorithm-11.py
@@ -0,0 +1,6 @@
+# Learning Algorithms
+# Aguiar - computer science
+# University Federal of Ceara - campus Crateus
+
+print("===== Learning Algorithms - Python =====")
+print("\nLearning algorithm.")
