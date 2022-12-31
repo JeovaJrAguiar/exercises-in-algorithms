4330616c7898d182f1e94eafdd0ca0d82cfa0cea
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-12-31 17:51:32 -0300

    .

---
+++ 11-20/algorithm-13.c
@@ -0,0 +1,12 @@
+// Learning Alrithms 3.0
+// Aguiar - computer science
+// University Federal of Ceara - campus Crateus
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("\n===== Learning Algorithms 3.0- C =====");
+    printf("\nLearning algorithm.");
+    printf("\nWith Anitta and Guto");
+}
---
+++ 11-20/algorithm-13.py
@@ -0,0 +1,7 @@
+# Learning Algorithms 3.0
+# Aguiar - computer science
+# University Federal of Ceara
+
+print("===== Learning Algorithms 3.0 - Python =====")
+print("\nLearning algorithm.")
+print("With Anitta and Guto.")
