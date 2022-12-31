f19fc4f8eec827b9d1a8b9aa1ed19f251c8065b2
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-12-31 01:08:51 -0300

    .

---
+++ 11-20/algorithm-12.c
@@ -0,0 +1,12 @@
+// Learning Algorithms 2.0
+// Aguiar - computer science
+// University Federal of Ceará - campus Crateus
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    printf("\n===== Learning Algorithms - C =====");
+    printf("Learning algorithm.");
+    printf("With Anitta and Guto");
+} 
