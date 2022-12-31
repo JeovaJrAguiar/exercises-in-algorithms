c66ae3063a17e5096f3b74704ef50fe45eb4d074
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-12-31 17:52:10 -0300

    chellenge-14-c-and-executable

---
+++ 11-20/algorithm-14.c
@@ -0,0 +1,12 @@
+// Learning Alrithms 4.0
+// Aguiar - computer science
+// University Federal of Ceara - campus Crateus
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("\n===== Learning Algorithms 4.0 - C =====\n");
+    printf("Learning algorithm.\n");
+    printf("With Anitta and Guto.\n");
+}
