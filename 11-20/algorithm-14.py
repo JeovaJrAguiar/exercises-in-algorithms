0ffbd4cf4a7bcb8e5a378e82d645ba738b47074a
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-12-31 17:51:52 -0300

    chellenge-14-python

---
+++ 11-20/algorithm-14.py
@@ -0,0 +1,7 @@
+# Learning Algorithms 4.0
+# Aguiar - computer science
+# University Federal of Ceara
+
+print("===== Learning Algorithms 4.0 - Python =====\n")
+print("Learning algorithm.\n")
+print("With Anitta and Guto.")
