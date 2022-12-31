54a03e5f5b83165ca0cc4ce72c1a0988d0801565
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-12-31 01:09:05 -0300

    .

---
+++ 11-20/algorithm-12.py
@@ -0,0 +1,7 @@
+# Learning Algorithmsc2.0
+# Aguiar - computer science
+# University Federal of Ceara - campus Crateus
+
+print("===== Learning Algorithms - Python =====")
+print("Learning algorithm.")
+print("With Anitta and Guto.")
