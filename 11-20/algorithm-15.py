ca1d17a63c0461112da82fefcfb3dff43b8e66cb
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-12-31 21:06:12 -0300

    .

---
+++ 11-20/algorithm-15.py
@@ -0,0 +1,6 @@
+# Learning Algorithms 5.0
+# Aguiar - computer science
+# University Federal of Ceara
+
+print("===== Learning Algorithms 4.0 - Python =====\n")
+print("Learning algorithm.\nLearning algorithm.\nWith Anitta and Guto.\nVery easy.\nWith Visual Studio Code.")
