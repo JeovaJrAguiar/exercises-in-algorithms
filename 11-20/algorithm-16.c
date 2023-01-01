f01c2cf6740a03e47ca26b6436f1a13976588e8f
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2023-01-01 20:48:34 -0300

    challenge-16-c

---
+++ 11-20/algorithm-16.c
@@ -0,0 +1,14 @@
+// Simple variable
+// Aguiar - computer science
+// University Federal of Ceará - campus Crateus
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("\n===== Simple variable  - C =====");
+    int x;
+    x = 10;
+
+    printf("\nx = %d\n", x);
+}
