5350cf8d38903dc82313a8ecde1f75e00d150d21
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-12-31 21:06:06 -0300

    .

---
+++ 11-20/algorithm-15.c
@@ -0,0 +1,13 @@
+// Learning Algorithms 5.0
+// Aguiar - computer science
+// University Federal of Ceará - campus Crateus
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("\n===== Learning Algorithms 5.0 - C =====");
+    printf("\nLearning algorithm.\nWith Anitta and Guto.\nVery easy.\nWith Visual Studio Code.")
+
+    return 0;
+}
