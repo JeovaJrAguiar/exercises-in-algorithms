b0b9e83275a3501799d607e05bdf9cb6d83d3776
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2023-01-02 15:04:14 -0300

    chellenge-17-python

---
+++ 11-20/algorithm-17.py
@@ -0,0 +1,10 @@
+# Simple variable using outPut
+# Aguiar - computer science
+# Federal University of Ceara
+
+print("===== Simple variable using outPut - Python =====")
+x = int()
+x = 10
+
+print("x value: {}".format(x))
+
