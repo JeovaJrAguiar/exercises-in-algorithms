8cc0e60f068db539b5aac30eed4da40fcd560169
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2023-01-02 15:03:51 -0300

    chellenge-17-c

---
+++ 11-20/algorithm-17.c
@@ -0,0 +1,16 @@
+// Simple variable using outPut
+// Aguiar - computer science
+// Federal Unidersity of Ceara
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+        printf("\n===== Simple variable using outPut - C =====\n");
+        int x = 0;
+        x = 10;
+
+        printf("x value: %d \n", x );
+
+        return 0;
+}
