9fe41fac2fa6bc8275434fd17a9a15d3265d4aaa
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2023-01-03 09:35:46 -0300

    challenge-18-python

---
+++ 11-20/algorithm-18.py
@@ -0,0 +1,9 @@
+# Simple algorithm using outPut with some
+# Aguiar - computer science
+# Federal Univesity of Ceara - campus crateus
+
+print("\n===== Simple variable using outPut with some - Python =====")
+
+x = int()
+x = 10
+print("X: {}\nY: X + 1 => {}\n\n".format(x, x+1))
