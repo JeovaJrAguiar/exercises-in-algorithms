6e3529a7311a5e3dd2364d5cf34fbafc857c3451
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2023-01-03 09:35:33 -0300

    challenge-18-c-and-executable

---
+++ 11-20/algorithm-18.c
@@ -0,0 +1,16 @@
+// Simple algorithm using outPut with some
+// Aguiar - computer science
+// Federal University of Ceara
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("\n===== Simple variable using outPut with some - C =====\n");
+
+	int x = 0;
+	x = 10;
+	printf("X: %d \nY: X + 1 => %d \n", x, x + 1);
+
+	return 0;
+}
