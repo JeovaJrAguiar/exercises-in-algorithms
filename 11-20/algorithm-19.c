927babc0431e766c892a5782536e7336ec144893
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2023-01-06 23:23:04 -0300

    challenge-19-c-and-executabe

---
+++ 11-20/algorithm-19.c
@@ -0,0 +1,28 @@
+// Basic operatons
+// Aguiar - computer science
+// Federal University of Ceara - crateus campus
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+float raiz(float numero){
+	float precisao = 0.000001;
+	float b = numero, a = 1;
+
+	while((b-a) >= precisao){
+		b = (a+b)/2;
+		a = numero / b;
+	}
+
+	return b;
+}
+
+int main(){
+	printf("\n===== Basic Operations - C =====\n");
+
+
+	printf("raiz: %.2f . raiz(64)\n", raiz(64));
+	
+	return 0;
+}
