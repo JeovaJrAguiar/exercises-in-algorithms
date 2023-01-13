8a34720fbe53f7f9118cccc045f33836b813212a
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2023-01-12 23:41:00 -0300

    algorithm-19-study

--- 11-20/algorithm-19.c
+++ 11-20/algorithm-19.c
@@ -1,4 +1,4 @@
-// Basic operatons
+// Basic operations
 // Aguiar - computer science
 // Federal University of Ceara - crateus campus
 
@@ -10,12 +10,16 @@
 float raiz(float numero){
 	float precisao = 0.000001;
 	float b = numero, a = 1;
-
+	
+	printf("\nprecisao - a        - b        - numero");
 	while((b-a) >= precisao){
 		b = (a+b)/2;
 		a = numero / b;
+
+		printf("\n%.6f - %.6f - %.6f - %f", precisao, a, b, numero);
 	}
 
+	printf("\n\n%.6f - %.6f - %.6f - %f\n", precisao, a, b, numero);
 	return b;
 }
 
@@ -23,7 +27,7 @@ int main(){
 	printf("\n===== Basic Operations - C =====\n");
 
 
-	printf("raiz: %.2f . raiz(64)\n", raiz(64));
-	
+	printf("raiz: %.2f . raiz(64)\n", raiz(9));
+
 	return 0;
 }
