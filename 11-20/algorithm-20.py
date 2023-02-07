a97f012350c2199fce6edcfce50f0ab202b129b1
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2023-02-07 10:38:22 -0300

    chellenge-20-in-python

---
+++ 11-20/algorithm-20.py
@@ -0,0 +1,12 @@
+// Basic operations
+// Aguiar - computer science
+// Federal University of Ceara - crateus campus
+
+print('\n===== String Manipulatoin - Python =====')
+word = input("Word: ")
+word2 = input("Word: ");
+wordConcat = word+word2
+
+print("Size first letter: {}".format(len(word)))
+print("Concatenation: {}".format(wordConcat))
+print("first letter: {}".format(wordConcat.split() [0])) 
