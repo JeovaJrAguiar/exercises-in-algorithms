37de065fa7803a1a93253ddc71c9ec718f086142
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2023-09-22 15:36:10 -0300

    Create 04-ex-02-string-comparison.sh

---
+++ shellScript/04-ex-02-string-comparison.sh
@@ -0,0 +1,9 @@
+#variavel                                                       
+texto='shell-AGUIAR'
+
+#compara se o valor na string text eh diferente de 'shell'      
+if [ '$string' != 'shell' ];then
+        echo 'Diferentes'
+else
+        echo 'Iguais'
+fi
