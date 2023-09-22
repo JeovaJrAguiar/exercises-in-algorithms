f2c24ded36b65758f1e54d6ed0c50c3e67493cb7
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2023-09-22 15:35:45 -0300

    Create 04-ex-01-string-comparison.sh

---
+++ shellScript/04-ex-01-string-comparison.sh
@@ -0,0 +1,9 @@
+File Edit Options Buffers Tools Sh-Script Help                  
+string='shell-aguiar-jr'
+
+# compara o valor armazenado em 'string' com 'shell-aguiar-jr'  
+if [ '$string' = 'shell-aguiar-jr' ];then
+        echo 'A string eh shell-aguiar-jr'
+else
+        echo 'Diferentes'
+fi
