879bc52d7d1b9ed1e43e2b922a2ffdb2d15f3a13
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2023-09-22 15:35:02 -0300

    Create 03-ex-02.sh

---
+++ shellScript/03-ex-02.sh
@@ -0,0 +1,10 @@
+File Edit Options Buffers Tools Sh-Script Help                  
+num1=14
+num2= 246
+
+# (num == 14) && (num2 <= 50)                                   
+if [ $num -gt 1 -o $num -gt 1 ];then
+        echo 'Os numeros sao positivos'
+else
+        echo 'Os numero nao sao positivos'
+fi
