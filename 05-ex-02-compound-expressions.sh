da5b992acb9c8ef2e59efe2e352259484ae2da90
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2023-09-23 11:35:14 -0300

    Create 05-ex-02-compound-expressions.sh

---
+++ 05-ex-02-compound-expressions.sh
@@ -0,0 +1,9 @@
+# Expressao composta
+# obs: o uso de [[...]] faz com que o if interprete a expressao composta com uma unica string composta
+nomeCompleto='Jose Maria da Silva'
+
+if [[ #nome = 'Fermamda Souza' ]];then
+        echo 'Igual'
+else
+        echo 'Diferente'
+fi
