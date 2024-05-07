137e3ffc51845eeec4d9f4911e1d33b27e030cb0
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2024-05-07 20:04:17 -0300

    cifra de nlogonia

--- brazilian-olymp-computation-python/nivel-2/2021/fase-1/cifra-da-nlogonia.py
+++ brazilian-olymp-computation-python/nivel-2/2021/fase-1/cifra-da-nlogonia.py
@@ -5,27 +5,29 @@ texto = input()
 
 cifra = ""
 
+# cifra uma consoante em uma tríade
 def cifrar(consoanteAtual):
     distDireita = 0
     distEsquerda = 0
     vogalProxima = ""
 
+    # procurando vogal à direita
     indiceConsoante = alfabeto.find(consoanteAtual)
-    # procurando a direita
     for letra in alfabeto[indiceConsoante+1:]:
         distDireita += 1
         if letra in vogais:
             break
     
+    # procurando vagal à esquerda
     consoanteSeguinte = ""
     alfabetoInvertido = alfabeto[::-1]
     indiceConsoanteInvertido = alfabetoInvertido.find(consoanteAtual)
-    # procurando a esquerda
     for letra in alfabetoInvertido[indiceConsoanteInvertido+1:]:
         distEsquerda += 1
         if letra in vogais:
             break
     
+    # definindo qual a vogal mais proxima
     if distEsquerda > distDireita:
         vogalProxima = alfabeto[indiceConsoante+distDireita]
     elif distEsquerda < distDireita:
@@ -33,13 +35,16 @@ def cifrar(consoanteAtual):
     else:
         vogalProxima = alfabeto[indiceConsoante-distEsquerda]
 
+    # define a consoonte seguinte
     for letra in alfabeto[indiceConsoante+1:]:
         if letra not in vogais:
             consoanteSeguinte = letra
             break
     
+    # retorna a concatenação dos 3 elementos
     return consoanteAtual + vogalProxima + consoanteSeguinte
 
+# percorre a entrada em busca de consoantes para poder cifrar as consoantes
 for letra in texto:
     if letra not in vogais:
         cifra += cifrar(letra)
