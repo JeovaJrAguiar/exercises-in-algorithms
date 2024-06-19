238167664b557ff59871c12355c3c556e93693c4
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2024-06-18 21:57:13 -0300

    Update 09-recursion-0

--- hacker-rank-python/30-days-of-code/09-recursion-3/09-recursion-3.py
+++ hacker-rank-python/30-days-of-code/09-recursion-3/09-recursion-3.py
@@ -14,10 +14,10 @@ import sys
 #
 
 def factorial(n):
-    if n==0:
+    if n==0 or n==1:
         return 0
     
-    return factorial(n-1)
+    return ( n* factorial(n-1) )
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
