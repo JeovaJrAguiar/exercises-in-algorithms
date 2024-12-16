dd283eab93a47c40317efde1be478942606a85c8
JeovaJrAguiar <aguiarjr897@gmail.com>
2024-12-16 20:56:12 -0300

    update selection-sort algorithm

--- selection-sort.py
+++ selection-sort.py
@@ -1,12 +1,19 @@
 # iteration version
 
+def selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        min_index = i
+
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
 arr = [90, 23, 4, 3342, 348, 1223, 5624, 32, 7, 3, 2, 2, 2, 1432, 3, 24, 646]
 
-def selection_sort(arr):
-    for i, value in enumerate(arr):
-        aux = value
+selection_sort(arr)
 
-        for j, content in enumerate(arr[i:]):
-            if value < content:
-                arr[i] = arr[j]
-                arr[j] = aux
+print(arr)
